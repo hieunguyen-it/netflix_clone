913e8e1d22e77b47f5c731f8f1df4df8b67bfa3f
Hieu Nguyen <hieunp@bibabo.vn>
2023-04-15 12:18:59 +0700

    Initial Commit

---
+++ NetFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetFlix
+//
+//  Created by Nguyen Phan Hieu on 4/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
